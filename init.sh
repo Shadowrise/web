641178dcc61747bcc9b8e6de03ea1127b538af91
Shadowrise <zellion@yandex.ru>
2019-01-24 22:36:51 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
   sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
   sudo rm -rf /etc/nginx/sites-enabled/default
   sudo /etc/init.d/nginx restart
-  sudo cd /home/box/web/ask
-  sudo python manage.py runserver 0.0.0.0:80000 
+  sudo python /home/box/web/ask/manage.py runserver 0.0.0.0:8000
