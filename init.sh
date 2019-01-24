194d8cacc267c9bc2cad62e521cf9bed4bea0d0c
Shadowrise <zellion@yandex.ru>
2019-01-24 22:24:09 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
-  git clone https://Shadowrise:Zona4567@github.com/Shadowrise/web.git /home/box/web
   sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
   sudo rm -rf /etc/nginx/sites-enabled/default
   sudo /etc/init.d/nginx restart
