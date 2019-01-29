3da14fd890b0e73193d10f9b605d741ee0d0f06e
Shadowrise <zellion@yandex.ru>
2019-01-29 22:00:17 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -14,14 +14,14 @@ class Question(models.Model):
   objects = QuestionManager()
   title = models.CharField(max_length=255)
   text = models.TextField()
-  added_at = models.DateTimeField(blank=True)
-  rating = models.IntegerField()
+  added_at = models.DateTimeField(auto_now_add=True)
+  rating = models.IntegerField(default=0)
   author = models.OneToOneField(User, related_name="author")
   likes = models.ManyToManyField(User, related_name="likes")
  
 class Answer(models.Model):
   text = models.TextField()
-  added_at = models.DateTimeField(blank=True)
+  added_at = models.DateTimeField(auto_now_add=True)
   question = models.OneToOneField(Question)
   author = models.OneToOneField(User)
   
