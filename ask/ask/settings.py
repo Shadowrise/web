20401bc54c2a92ae4f0e6093a665c5073d0c5cee
Shadowrise <zellion@yandex.ru>
2019-01-29 21:24:20 +0300

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'MYDB',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+    }
     }
 }
 
