87fc0efffdee23545cd8e046d586e7f39bb7b52b
Shadowrise <zellion@yandex.ru>
2019-01-31 21:17:52 +0300

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,12 +1,8 @@
 from django.conf.urls import url
-from qa.views import test
+from qa.views import new, popular, one
 
 urlpatterns = [
-  url(r'^$', test),
-  url(r'^login/$', test),
-  url(r'^signup/$', test),
-  url(r'^question/[0-9]+/$', test),
-  url(r'^ask/$', test),
-  url(r'^popular/$', test),
-  url(r'^new/$', test),
+  url(r'^$', new),
+  url(r'^popular/$', popular),
+  url(r'^question/(?P<question_id>\d+)$', test),
 ]
