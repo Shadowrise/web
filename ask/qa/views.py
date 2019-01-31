28b6f57dc22372cfe496a3bc45dff89b2729d7b6
Shadowrise <zellion@yandex.ru>
2019-01-31 21:36:44 +0300

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,3 +1,40 @@
-from django.http import HttpResponse 
-def test(request, *args, **kwargs):
-    return HttpResponse('OK')
+from django.http import HttpResponse, Http404 
+from django.core.paginator import Paginator
+
+def paginate(request, qs):
+    try:
+        limit = int(request.GET.get('limit', 10))
+     except ValueError:
+        raise Http404
+     
+    if limit > 100:
+        limit = 10
+        
+    try:
+        page = int(request.GET.get('page', 1))
+    except ValueError:
+        raise Http404        
+        
+    paginator = Paginator(qs, limit)
+    try:
+        page = paginator.page(page)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
+    return page
+
+def new(request):
+    qs = Question.objects.all()
+    page = paginate(request, qs)    
+    data = {
+        'questions': page.object_list,
+        'paginator': paginator,
+        'page': page,
+    }
+    return render(request, 'new.html', data)
+
+def popular(request):
+    return HttpResponse('')
+
+def one(request, question_id):
+    return HttpResponse('')
+
