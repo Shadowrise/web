a2428dc47f697d53338b626ef3b04988ed9f6188
Shadowrise <zellion@yandex.ru>
2019-01-31 22:21:43 +0300

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -4,5 +4,5 @@ from qa.views import new, popular, one
 urlpatterns = [
   url(r'^$', new),
   url(r'^popular/$', popular),
-  url(r'^question/(?P<question_id>\d+)$', one),
+  url(r'^question/(?P<question_id>\d+)/$', one),
 ]
