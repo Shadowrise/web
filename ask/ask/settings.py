280f0474f183043fd94c1f17b6dac49672419c92
Shadowrise <zellion@yandex.ru>
2019-02-03 14:34:23 +0300

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'mydb',
-        'USER': 'root',
+        'USER': 'box',
         'PASSWORD': '',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
