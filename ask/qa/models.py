32eae2d71c92547f339845e0f2f38aacee85e836
Shadowrise <zellion@yandex.ru>
2019-02-03 16:41:31 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -28,6 +28,6 @@ class Question(models.Model):
 class Answer(models.Model):
   text = models.TextField()
   added_at = models.DateTimeField(auto_now_add=True)
-  question = models.OneToOneField(Question)
+  question = models.ForeignKey(Question)
   author = models.ForeignKey(User)
   
