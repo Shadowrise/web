066f3cd338357d1020afccb9dcbf645db762b35e
Shadowrise <zellion@yandex.ru>
2019-02-03 14:46:23 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
   sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
   sudo rm -rf /etc/nginx/sites-enabled/default
   sudo /etc/init.d/nginx restart
+  sudo /etc/init.d/mysql restart
+  sudo mysql -u root < /home/box/web/init.sql
   sudo python /home/box/web/ask/manage.py runserver 0.0.0.0:8000
