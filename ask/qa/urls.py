c0cac120a9442bb623d94b669350c3c6fec29188
Shadowrise <zellion@yandex.ru>
2019-02-04 21:56:07 +0300

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
-from qa.views import new, popular, one, ask
+from qa.views import new, popular, one, ask, signup, login
 
 urlpatterns = [
   url(r'^$', new),
   url(r'^popular/$', popular),
   url(r'^ask/$', ask),
+  url(r'^signup/$', signup),
+  url(r'^login/$', login),
   url(r'^question/(?P<id>\d+)/$', one),
 ]
