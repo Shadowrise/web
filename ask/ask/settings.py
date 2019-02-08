2e3c80106097781798b3cf07c01d245e0dc167a1
Shadowrise <zellion@yandex.ru>
2019-02-09 00:14:53 +0300

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'qa.middleware'
 ]
 
 ROOT_URLCONF = 'ask.urls'
