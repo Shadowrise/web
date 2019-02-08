a46ce64f6636495aa01268791b33bf23894c7095
Shadowrise <zellion@yandex.ru>
2019-02-09 00:26:26 +0300

    Update middleware.py

--- ask/qa/middleware.py
+++ ask/qa/middleware.py
@@ -1,4 +1,11 @@
 class CheckSessionMiidleware(object):
+  
+  def __init__(self, get_response):
+        self.get_response = get_response
+
+  def __call__(self, request):
+      return self.get_response(request)
+    
   def process_request(self, request):
     try:
       sessid = request.COOKIE.get('sessid')
