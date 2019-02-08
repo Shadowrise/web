31d0c17be566496f6fefc0e2f2f611d6447fb04b
Shadowrise <zellion@yandex.ru>
2019-02-09 00:13:34 +0300

    Create middleware.py

---
+++ ask/qa/middleware.py
@@ -0,0 +1,10 @@
+class CheckSessionMiidleware(class):
+  def process_request(request):
+    try:
+      sessid = request.COOKIE.get('sessid')
+      session = Session.objects.get(key=sessid, expires__gt=datetime.now(),)
+      request.session = session
+      request.user = session.user
+    except Session.DoesNotExist:
+      request.session = None
+      request.user = No
